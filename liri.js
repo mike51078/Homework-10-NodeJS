cd7e21747618cf95997607f38238996359dd4b6a
elcid00 <elcid00@aol.com>
2019-03-22 15:58:01 -0400

    still working on setup

--- liri.js
+++ liri.js
@@ -1,1 +1,10 @@
+
+var spotify = require("node-spotify-api")
+var axios = require("axios");
+var moment = require("moment");
+var dotenv = require("dotenv");
+var keys = require('./keys.js');
+var fs = require('fs');
+var spotify = new spotify(keys.spotify);
 require("dotenv").config();
+
